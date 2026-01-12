5d967c1a14038e1331e3f8660832c5be7ce95aa5
PedroPepeu <pedrosteamgtav@gmail.com>
2026-01-12 01:54:49 -0300

    Scope: Creating main loop + ADD OPcode logic
    Create the main function to run the main loop inside of it and
    created an switch case to each of the opcodes. To support the ADD
    opcode, I created the sign_extend and the update_flags functions
    the `ADD` will take two values and stores them in a register. When in
    immediate mode, the second value is embedded in the right-most 5 bit
    of the instruction. Values which are shorter than 16 bits, it needs
    to use the `sign_extend` function. At any time an instruction
    modifies a register, the condition flags need to be updated.
    
    Signed-off-by: PedroPepeu <pedrosteamgtav@gmail.com>

--- lc3.c
+++ lc3.c
@@ -53,3 +53,153 @@ enum {
     OP_LEA,     /* load effective address */
     OP_TRAP     /* execute trap */
 };
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
+
+int main(int argc, const char* argv[])
+{
+    // Load arguments
+ 
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    // Setup
+
+    /* Since exactly one condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
+
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+        case OP_ADD:
+            /* destination register (DR) */
+            uint16_t r0 = (instr >> 9) & 0x7;
+            /* first operand (SR1) */
+            uint16_t r1 = (instr >> 6) & 0x7;
+            /* whether we are in the immediate mode */
+            uint16_t imm_flag = (instr >> 5) & 0x1;
+
+            if (imm_flag)
+            {
+                uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                reg[r0] = reg[r1] + imm5;
+            }
+            else
+            {
+                uint16_t r2 = instr & 0x7;
+                reg[r0] = reg[r1] + reg[r2];
+            }
+
+            update_flags(r0);
+            break;
+
+        case OP_AND:
+            /* code */
+            break;
+
+        case OP_NOT:
+            /* code */
+            break;
+
+        case OP_BR:
+            /* code */
+            break;
+
+        case OP_JMP:
+            /* code */
+            break;
+
+        case OP_JSR:
+            /* code */
+            break;
+
+        case OP_LD:
+            /* code */
+            break;
+
+        case OP_LDI:
+            /* code */
+            break;
+
+        case OP_LDR:
+            /* code */
+            break;
+
+        case OP_LEA:
+            /* code */
+            break;
+
+        case OP_ST:
+            /* code */
+            break;
+
+        case OP_STI:
+            /* code */
+            break;
+
+        case OP_STR:
+            /* code */
+            break;
+
+        case OP_TRAP:
+            /* code */
+            break;
+
+        case OP_RES:
+            /* code */
+
+        case OP_RTI:
+            /* code */
+
+        default:
+            break;
+        }
+    }
+    // Shutdown
+}
